4d5adbf29ebbf0495e09ad0666fc13207b4f1a13
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2026-05-06 10:52:52 +0700

    changed to only get the published post only instead of all of it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,17 +15,18 @@ import Carousel from 'react-bootstrap/Carousel'
 
 function Home() {
 
-  const [allPosts, setAllPosts] = useState()
+  const [publishedPosts, setPublishedPosts] = useState()
   const [isLoading, setIsLoading] = useState(true)
 
   const { setUser } = useContext(UserContext)
 
   useEffect(() => {
-    const retrieveAllPosts = async () => {
+    const retrievepublishedPosts = async () => {
       try {
-        const retrieveResponse = await api.get('/all-posts')
+        const retrieveResponse = await api.get('/published-posts')
         if (retrieveResponse.status === 200) {
-          setAllPosts(retrieveResponse.data.allPosts)
+          console.log(retrieveResponse.data.publishedPosts)
+          setPublishedPosts(retrieveResponse.data.publishedPosts)
         }
       } catch (err) {
         console.error(err)
@@ -34,7 +35,7 @@ function Home() {
       }
     }
     auth(setUser)
-    retrieveAllPosts()
+    retrievepublishedPosts()
   }, [])
 
   return (
@@ -45,30 +46,30 @@ function Home() {
         <Row>
           <Carousel className="mt-4 " data-bs-theme="light">
             <Carousel.Item>
-              <Link to={`/post/${allPosts[0].id}`}>
-                <Image className="object-fit-cover h-md-50" src={allPosts[0].postPicture} height={600} width="100%" alt="first slide" rounded/>
+              <Link to={`/post/${publishedPosts[0].id}`}>
+                <Image className="object-fit-cover h-md-50" src={publishedPosts[0].postPicture} height={600} width="100%" alt="first slide" rounded/>
               </Link>
               <Carousel.Caption className="position-absolute bottom-0 start-0 end-0 m-0 p-4 bg-dark bg-opacity-75 text-start">
-                <h5 className="fw-bold text-white mb-1">{allPosts[0].title}</h5>
-                <p className="text-white-50 small mb-0">{allPosts[0].subtitle}</p>
+                <h5 className="fw-bold text-white mb-1">{publishedPosts[0].title}</h5>
+                <p className="text-white-50 small mb-0">{publishedPosts[0].subtitle}</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <Link to={`/post/${allPosts[0].id}`}>
-                <Image className="object-fit-cover h-md-50" src={allPosts[1].postPicture} height={600} width="100%" alt="second slide" rounded/>
+              <Link to={`/post/${publishedPosts[0].id}`}>
+                <Image className="object-fit-cover h-md-50" src={publishedPosts[1].postPicture} height={600} width="100%" alt="second slide" rounded/>
               </Link>
               <Carousel.Caption className="position-absolute bottom-0 start-0 end-0 m-0 p-4 bg-dark bg-opacity-75 text-start">
-                <h5 className="fw-bold text-white mb-1">{allPosts[1].title}</h5>
-                <p className="text-white-50 small mb-0">{allPosts[1].subtitle}</p>
+                <h5 className="fw-bold text-white mb-1">{publishedPosts[1].title}</h5>
+                <p className="text-white-50 small mb-0">{publishedPosts[1].subtitle}</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <Link to={`/post/${allPosts[2].id}`}>
-                <Image className="object-fit-cover h-md-50" src={allPosts[2].postPicture} height={600} width="100%" alt="second slide" rounded/>
+              <Link to={`/post/${publishedPosts[2].id}`}>
+                <Image className="object-fit-cover h-md-50" src={publishedPosts[2].postPicture} height={600} width="100%" alt="second slide" rounded/>
               </Link>
               <Carousel.Caption className="position-absolute bottom-0 start-0 end-0 m-0 p-4 bg-dark bg-opacity-75 text-start">
-                <h5 className="fw-bold text-white mb-1">{allPosts[2].title}</h5>
-                <p className="text-white-50 small mb-0">{allPosts[2].subtitle}</p>
+                <h5 className="fw-bold text-white mb-1">{publishedPosts[2].title}</h5>
+                <p className="text-white-50 small mb-0">{publishedPosts[2].subtitle}</p>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
@@ -80,7 +81,7 @@ function Home() {
           <Row className="pt-5">
             <Col className="col-lg-9">
               <h3 className="pb-3">Latest Articles</h3>
-                {allPosts.map((post) => (
+                {publishedPosts.map((post) => (
                   <ArticleCard post={post} key={post.id} />
                 ))}
             </Col>
